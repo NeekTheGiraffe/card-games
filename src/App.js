2bb6c6416230befac643ac7bd006355fb7ba6e75
Brian Fang <brianfang@ucla.edu>
2022-05-14 15:26:23 -0700

    removed old/unused files, improved navbar css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,92 @@
-import React from 'react';
+import 'bulma/css/bulma.css'
 import './App.css';
+//for css styling
 
-import Home from './Home';
-import Profile from './Profile';
+import React, { useRef, useState, Suspense } from 'react';
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+import 'firebase/compat/analytics';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+import Home from './Home.js';
+import Profile from './Profile.js';
+import { SuspenseWithPerf } from 'reactfire';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyC4AHzhZgQfeBd9Nt2ZlzkuWBoEsJuy28U",
+  authDomain: "card-games-f326f.firebaseapp.com",
+  projectId: "card-games-f326f",
+  storageBucket: "card-games-f326f.appspot.com",
+  messagingSenderId: "425180528217",
+  appId: "1:425180528217:web:a63236d19213afd9df3b48",
+  measurementId: "G-74W2Z4T1KC"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
 
 function App() {
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <Home />
-      <Profile />
+      <SuspenseWithPerf fallback={<p>Loading...</p>}>
+        <header>
+          <Navbar />
+        </header>
+
+        <section>
+          {user ? <SignOut /> : <SignIn />}
+        </section>
+      </SuspenseWithPerf>
     </div>
   );
 }
+
+function SignIn() {
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+    </>
+  )
+
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+
+function Navbar() {
+  return (
+    <nav class='navbar'>
+      <div class='navbar-brand'>
+        <a class='navbar-item'>The Card Game Cowboys</a>
+      </div>
+      <div class='navbar-menu'>
+        <div class='navbar-start'></div>
+        <div class='navbar-end'>
+          <a class="navbar-item">Home</a>
+          <a class="navbar-item">Profile Page</a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
 
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Home(){
     return(
         <div>
-            <h1>Welcome</h1>
+            <h1>Home</h1>
         </div>
     );
 }
