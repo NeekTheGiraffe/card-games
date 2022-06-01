64fdacd39050f602d402cf6f873b37b497da6be0
Aram Torosyan <aramtorosyan1999@gmail.com>
2022-05-31 17:03:08 -0700

    Memory game has been added

---
+++ src/Memory Game/index.js
@@ -0,0 +1,237 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import cardBack from './cardBack.jpg';
+import banner from './banner.jpg';
+import four from './four.png';
+import Jack from './Jack.png';
+import Joker from './Joker.jpg';
+import King from './King.png';
+import Queen from './Queen.png';
+import suits from './suits.jpg';
+import ten from './ten.png';
+import circle from './circle.png';
+
+class Square extends React.Component {
+    render() {
+        return (
+            <button className="square"
+               onClick = {() => this.props.onClick()}>
+              {images[this.props.value]}
+            </button>
+        );
+  }
+}
+
+let initialFlip = [];
+let isOver=[];
+for(let i=0; i < 16; i++) {
+    initialFlip[i] = false;
+    isOver[i]=false;
+}
+  
+class Board extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            numberOfClicks: 0,
+            numberOfFound: 0,
+            previousClick2: null,
+            previousClick: null,
+            numbers: randImages(),
+            isFliped: initialFlip,
+            winner: null,
+            isOver: initialFlip
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(i) {
+        this.setState({numberOfClicks: (this.state.numberOfClicks+1)});
+        if(this.state.previousClick2 === null) {
+            initialFlip[i]=true;
+            this.setState({isFliped: initialFlip,
+                        previousClick: i});
+        } else {
+            initialFlip[i]=true;
+            if(!(this.state.numbers[this.state.previousClick2] ===
+                this.state.numbers[i])) {
+            initialFlip[this.state.previousClick2]=false;
+            this.setState({isFliped: initialFlip,
+                        previousClick: i,
+                        previousClick2: null});
+            } else {
+                isOver[this.state.previousClick2]=true;
+                isOver[i]=true;
+                this.setState({numberOfFound: (this.state.numberOfFound+1)});
+                initialFlip[i]=true;
+                this.setState({isFliped: initialFlip,
+                            isOver: isOver,
+                            previousClick: null,
+                            previousClick2: null})
+            }
+        }
+    }
+
+    handleClcikToInitialState(i) {
+        this.setState({numberOfClicks: (this.state.numberOfClicks+1)});
+        initialFlip[i]=true;
+
+        if (!(this.state.numbers[this.state.previousClick] ===
+            this.state.numbers[i])) {
+            initialFlip[this.state.previousClick]=false;
+            this.setState({isFliped: initialFlip,
+                previousClick: null,
+                previousClick2: i});
+        }
+        else {
+            isOver[this.state.previousClick]=true;
+            isOver[i]=true;
+            this.setState({numberOfFound: (this.state.numberOfFound+1)});
+            initialFlip[i]=true;
+            this.setState({isFliped: initialFlip,
+                            isOver: isOver,
+                            previousClick: null,
+                            previousClick2: null})
+        }
+    }
+
+    renderSquare(i) {
+        let value;
+        if(!this.state.isFliped[i]) {
+            value=0;
+        }
+        else {
+            value=this.state.numbers[i];
+        }
+      return ( <Square 
+        onClick={() => {
+            if(this.state.previousClick == null) {
+                if(!this.state.isOver[i]) {
+                    this.handleClick(i);
+                } else {
+                    return;
+                }
+            } 
+            else {
+                if(!this.state.isOver[i]){
+                    this.handleClcikToInitialState(i);
+                } else {
+                    return;
+                }
+            } 
+          }
+        }
+        value={value}
+      /> );
+    }
+  
+    render() {
+        let grid = [];
+
+        for (let i=0; i <= 3; i++) {
+            let gridrow = [];
+            for (let j = (4*i); j <= (3+4*i); j++) {
+                gridrow.push(this.renderSquare(j));
+            }
+
+            grid.push(<div className="board-row">
+                        {gridrow}
+                    </div>);
+        }
+            
+        return (<div className='grid'>
+                <div>
+                    {grid}
+                </div>
+                <div className="statistics">
+                    <p1>
+                    <br></br>
+                        The number of clicks: {this.state.numberOfClicks}
+                    <br></br>
+                        The number of pairs found: {this.state.numberOfFound}
+                    <br></br>
+                        There are {8-this.state.numberOfFound} pairs 
+                        left to find!
+                    <br></br>
+                    </p1>
+                </div>
+            </div>);
+
+    }
+}
+
+class MemoryGame extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                    <p1 className="rules">
+                        <br></br>
+         The rules for this memory game are pretty simple. 
+         The game board contains 8 pairs of two identical 
+         cards. Once clicked on the card, it is flipped. If 
+         two consecutive flips are the same card then a pair 
+         is found, otherwise the card previously flipped is 
+         being flipped back. The game is over once all the 
+         pairs are found.</p1>
+                </div>
+            </div>
+        )
+    }
+}
+
+const images = {
+    9: <img src={circle} alt="clear"
+    width="100" height="140"/>, 
+    0: <img src={cardBack} alt="back of a card"
+    width="100" height="140"/>,
+    1: <img src={banner} alt="back of a card"
+    width="100" height="120"/>,
+    2: <img src={four} alt="back of a card"
+    width="100" height="140"/>,
+    3: <img src={Jack} alt="back of a card"
+    width="100" height="130"/>,
+    4: <img src={Joker} alt="back of a card"
+    width="100" height="140"/>,
+    5: <img src={King} alt="back of a card"
+    width="95" height="140"/>,
+    6: <img src={Queen} alt="back of a card"
+    width="100" height="140"/>,
+    7: <img src={suits} alt="back of a card"
+    width="100" height="140"/>,
+    8: <img src={ten} alt="back of a card"
+    width="95" height="140"/>
+}
+
+
+function randImages() {
+    let numbers = [];
+    for (let i = 0; i < 8; i++) {
+        numbers[i] = i + 1;
+    }
+    for (let i =8; i < 16; i++) {
+        numbers[i] = i - 7;
+    }
+
+    function shuffleArray (array) {
+        for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          const temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+        }
+
+        return array;
+    }
+
+    numbers = shuffleArray(numbers);
+
+    return numbers;
+}
+
+
+export default MemoryGame;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,49 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import MemoryGame from './Memory Game/index.js';
 import reportWebVitals from './reportWebVitals';
+import {NavLink } from 'react-router-dom';
+
+const Home = () => (
+  <Router>
+    <div className="menu">
+      <div>
+        <ul>
+          <NavLink to="/app" className="toolbar">
+            Black Jack
+          </NavLink>
+          <NavLink to="/memoryGame" className="toolbar">
+            Memory Game
+          </NavLink>
+        </ul>
+      </div>
+      <Routes>
+        <Route path="/app" element={<App />} />
+        <Route path="/memoryGame" element={<MemoryGame />} />
+      </Routes>
+    </div>
+  </Router>
+);
+
+
+
+
+class Game extends React.Component {
+  render() {
+      return (
+      <React.StrictMode>
+        <Home />
+      </React.StrictMode>
+      );
+}
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Game />
 );
 
 // If you want to start measuring performance in your app, pass a function
